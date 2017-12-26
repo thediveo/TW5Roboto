93eab70d38d47ca4ca0674f88b9fe72334d75268
Harald Albrecht <hidden@hidden.org>
2017-12-26 19:41:02 +0100

    fixes release scripts; removes stale skeleton release files; adds roboto plugin release files

--- release.cmd
+++ release.cmd
@@ -2,8 +2,8 @@
 rem release.cmd
 rem
 rem generates release files:
-rem   editions\release\output\TW5Roboto.html -- the plugin demo wiki
-rem   editions\release\output\TW5Roboto.tid -- the plugin tiddler
+rem   editions\release\output\tw5roboto.html -- the plugin demo wiki
+rem   editions\release\output\tw5roboto.tid -- the plugin tiddler
 rem
 tiddlywiki ^
 	editions\release ^
